b2a03a74d57cb44783eb18f1043b7c84d2e8ce10
Dachang Liu <dachang_liu@mlcn000298dliu.local>
2018-09-20 11:45:33 +0800

    site init

---
+++ sitev2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import IndexBanner from './components/IndexBanner';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <IndexBanner/>
+      </div>
+    );
+  }
+}
+
+export default App;
