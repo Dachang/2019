69d63a799a3f76bdfe4a493c1278cb7dbfab370e
Dachang Liu <dachang_liu@mlcn000298dliu.local>
2019-01-07 11:36:56 +0800

    add static files

---
+++ sitev2/src/components/Marquee.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import '../styles/Marquee.css';
+
+export default class Marquee extends PureComponent {
+
+  render() {
+    return (
+      <div className="marquee">
+        <h1>
+        Hi, I’m Dachang,<br></br>a digital experience designer & developer
+        based in Beijing, China.
+        </h1>
+      </div>
+    );
+  }
+
+}
